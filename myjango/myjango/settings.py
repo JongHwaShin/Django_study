f271cf547251e3910beedf8ce05e2871b52fcecc
JongHwa Shin <tls1403@naver.com>
2022-02-10 17:47:50 +0900

    Django commit(2022_02_10)

--- myjango/myjango/settings.py
+++ myjango/myjango/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,STATIC_URL) ##BASE_DIRㅎ은 현재 폴더
+STATIC_ROOT = os.path.join(BASE_DIR,STATIC_URL) ##BASE_DIR은 현재 폴더
