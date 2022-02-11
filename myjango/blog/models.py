4be4d201941587503eaf2bf09704c0917c0ae76f
JongHwa Shin <tls1403@naver.com>
2022-02-11 13:07:43 +0900

    Django commit(2022_02_10)

--- myjango/blog/models.py
+++ myjango/blog/models.py
@@ -12,8 +12,13 @@ class Post(models.Model): # 글 클래스 만들기
     created_date = models.DateTimeField(default=timezone.now)
     #게시일
     published_date = models.DateTimeField(blank=True, null=True)
+    # #임의컬럼
+    # test = models.TextField()
+
+    #published_data 필드에 현재날짜를 저장하는 메서드
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+    #글 저장할때 제목 저장
     def __str__(self):
-        return self.title
+        return self.title+ '(' +str(self.id) + ')'
